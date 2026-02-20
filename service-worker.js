b72a35ddb86672e3eea096f82d1bcaa4629e7f80
kyul0822 <kyul0822@gmail.com>
2026-02-20 10:21:01 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('brain-app').then(function(cache) {
+      return cache.addAll(['index.html', 'manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
